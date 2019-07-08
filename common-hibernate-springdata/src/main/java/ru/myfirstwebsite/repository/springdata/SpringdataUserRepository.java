c28ccd3a5b906dc1ff320b567781fe0fc0b072b3
qweetar <44614864+qweetar@users.noreply.github.com>
2019-07-08 07:59:29 +0300

    Update SpringdataUserRepository.java

--- common-hibernate-springdata/src/main/java/ru/myfirstwebsite/repository/springdata/SpringdataUserRepository.java
+++ common-hibernate-springdata/src/main/java/ru/myfirstwebsite/repository/springdata/SpringdataUserRepository.java
@@ -12,33 +12,7 @@ public interface SpringdataUserRepository extends
 
     HibernateUser findByLogin(String login);
     
-//    List<HibernateUser> findByUserNameAnAndUserSurName(String userName, String userSurname);
-//
-//    List<HibernateUser> findDistinctByUserNameLikeOrderByRoleAscUserIdDesc(String userName);
-//
-//
-//
-//    @Query("select u from HibernateUser u")
-//    List<HibernateUser> findAllWightQuery();
-//
-//    @Query(value = "select u.* from HibernateUser u", nativeQuery = true)
-//    List<HibernateUser> nativeQuery();
-//
-//    @Query("select u from HibernateUser u where u.userId > :lowerId")
-//    List<HibernateUser> findUserWithLowerId(String lowerId);
-//
-//    @Query("select u.userId, r.roleId from HibernateUser u join HibernateRole r on.userId = r.user.userId > :lowerId")
-//    List<Object[]> findUserIdAndRoleId(@Param("lowerId") String param);
-//
-//    @Query("select u.userName from HibernateUser u where u.userId > :lowerId")
-//    List<String> findUsersNameWithLowerId (@Param("lowerId") String param);
-//
-//    @Query("select u.userId, u.email from HibernateUser u where u.userId > :lowerId")
-//    List<Object[]> findUserEmaliWithLowerId(@Param("lowerId") String param);
-//
-//    @Modifying
-//    @Query("update HibernateUser u set u = :u")
-//    int updateUserPhoneNumber(@Param("mobilePhone") HibernateUser user);
+
 
 
     HibernateUser findByUserId(Long id);
